1543e0909bbb4310a7c6fd266c53efde867c4464
shailendra <shail5788@gmail.com>
2019-10-09 11:02:17 +0530

    add ammendment in file upload

--- src/app/file-transfer/file-transfer.component.ts
+++ src/app/file-transfer/file-transfer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FileUpload } from "../models/upload.model";
+import { FileUploadService } from "../shared/file-upload.service";
 
 @Component({
   selector: "app-file-transfer",
@@ -7,15 +7,32 @@ import { FileUpload } from "../models/upload.model";
   styleUrls: ["./file-transfer.component.css"]
 })
 export class FileTransferComponent implements OnInit {
-  constructor() {}
+  getFile;
+  constructor(private fs: FileUploadService) {}
   model = { file: File, email: "", subject: "", quality: false, litmus: false };
 
   ngOnInit() {}
   onSelectFile(event) {
     console.log(event);
     this.model.file = event.target.files[0];
+    const files = event.target.files[0];
+    this.getFile = files;
   }
   onSubmit(form) {
+    const formData = new FormData();
+    formData.append("file", this.getFile);
+    formData.append("email", this.model.email);
+    formData.append("subject", this.model.subject);
+    formData.append("quality", JSON.stringify(this.model.quality));
+    formData.append("litmus", JSON.stringify(this.model.litmus));
+    this.fs.upload(formData).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    );
     console.log(form);
     console.log(this.model);
   }
--- src/app/file-transfer/file-transfer.module.ts
+++ src/app/file-transfer/file-transfer.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from "@angular/forms";
 import { FileTransferComponent } from "./file-transfer.component";
 import { FileTranserRoutingModule } from "./file-transfer.route.module";
 import { AuthGuard } from "../auth/auth.guard";
+import { FileUploadService } from "../shared/file-upload.service";
 @NgModule({
   declarations: [FileTransferComponent],
   imports: [FileTranserRoutingModule, FormsModule, CommonModule],
-  providers: [AuthGuard]
+  providers: [AuthGuard, FileUploadService]
 })
 export class FileTransferModule {}
